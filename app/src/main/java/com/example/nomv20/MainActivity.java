4fa6b66eab140986974e38b81a0220b26c57c6fd
yuna17c <57446370+yuna17c@users.noreply.github.com>
2021-01-16 15:41:18 -0500

    Added main screen

--- app/src/main/java/com/example/nomv20/MainActivity.java
+++ app/src/main/java/com/example/nomv20/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.nomv20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton imageButton4 = findViewById(R.id.imageButton4);
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                startActivity(new Intent(MainActivity.this, VegetableBasketActivity.class));
+            }
+        });
+
     }
 }

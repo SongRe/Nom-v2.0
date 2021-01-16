8760c8b010966627e1893e1ff88effd1a1204d13
SongRe <songks712@gmail.com>
2021-01-16 15:50:34 -0500

    fixed merge conflicts

--- app/src/main/java/com/example/nomv20/MainActivity.java
+++ app/src/main/java/com/example/nomv20/MainActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.view.View;
-=======
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -48,16 +47,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
 
         button = findViewById(R.id.button1);
-        button.setOnClickListener( new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
                         //permission not granted, request permissions
-                        String[] permissions =  {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
                         requestPermissions(permissions, PERMISSION_CODE);
                     } else {
                         //permission granted
@@ -69,37 +67,41 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    
-    private void pickImageFromGallery(){
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, IMAGE_PICK_CODE);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            Intent intent = new Intent(this, ConfirmActivity.class);
-            intent.putExtras(data);
-            startActivity(intent);
+        private void pickImageFromGallery () {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent, IMAGE_PICK_CODE);
+        }
+
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+            if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+                Intent intent = new Intent(this, ConfirmActivity.class);
+                intent.putExtras(data);
+
+                startActivity(intent);
+
+            }
 
         }
 
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch(requestCode) {
-            case PERMISSION_CODE:
-                if(grantResults.length > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //permission granted
-                    pickImageFromGallery();
-                } else {
-                    Toast.makeText(this, "Permission denied lol wut did u expect", Toast.LENGTH_LONG);
-                }
+        @Override
+        public void onRequestPermissionsResult ( int requestCode, @NonNull String[] permissions,
+        @NonNull int[] grantResults){
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            switch (requestCode) {
+                case PERMISSION_CODE:
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        //permission granted
+                        pickImageFromGallery();
+                    } else {
+                        Toast.makeText(this, "Permission denied lol wut did u expect", Toast.LENGTH_LONG);
+                    }
+            }
         }
-    }
+
 }
 
